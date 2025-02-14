4fa2134e50b0813e6f2663776cacfcf53b14f2eb
JeetLi <jeetwork168@gmail.com>
2025-02-14 03:53:48 +0300

    feat: add inital task data, fulfilled the second point

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,148 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
 
-export default function Home() {
+import { useEffect, useState, useRef } from "react";
+import "./page.css";
+
+type Mode = "add" | "remove";
+
+const useRenderCounter = () => {
+  const counter = useRef(0);
+  counter.current++;
+
+  return counter.current;
+};
+
+const RenderCountLabel = ({
+  count,
+  label,
+}: {
+  count: number;
+  label: string;
+}) => {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      {label} renders: <span style={{ color: "red" }}>{count}</span>
+    </div>
+  );
+};
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+const AddToEndButton = ({ onClick }: { onClick: () => void }) => {
+  const counter = useRenderCounter();
+  return (
+    <div className="button">
+      <button onClick={onClick}>Add to end</button>
+      <RenderCountLabel label="Button" count={counter} />
+    </div>
+  );
+};
+
+const AddToStartButton = ({ onClick }: { onClick: () => void }) => {
+  const counter = useRenderCounter();
+  return (
+    <div>
+      <button onClick={onClick}>Add to start</button>
+      <RenderCountLabel label="Button" count={counter} />
+    </div>
+  );
+};
+
+const ChangeModeButton = ({
+  action,
+  onClick,
+}: {
+  action: Mode;
+  onClick: () => void;
+}) => {
+  const counter = useRenderCounter();
+  return (
+    <div>
+      <button onClick={onClick}>change mode: {action}</button>
+      <RenderCountLabel label="Button" count={counter} />
+    </div>
+  );
+};
+
+const ListItem = ({
+  item,
+  index,
+  onRemove,
+}: {
+  item: string;
+  index: number;
+  onRemove: (index: number) => void;
+}) => {
+  const onClick = () => {};
+  return (
+    <li onClick={onClick} className="li-item">
+      {item}
+      <button className="btn-remove" onClick={() => onRemove(index)}>
+        x
+      </button>
+    </li>
+  );
+};
+
+let index = 0;
+
+const List = () => {
+  const counter = useRenderCounter();
+  const [items, setItems] = useState<string[]>([]);
+  const [action, setAction] = useState<Mode>("add");
+
+  const handleChangeAction = () => {
+    setAction((prev) => (prev === "add" ? "remove" : "add"));
+  };
+
+  const handlRemoveItems = () => {
+    setItems((prev) => prev.slice(0, prev.length - 1));
+  };
+
+  const handleRemoveItem = (index: number) => {
+    return setItems(items.filter((_, i) => i !== index));
+  };
+
+  const handleAddItem = () => {
+    index++;
+    setItems((prev) => [...prev, `${index}-item`]);
+  };
+
+  const handleAddToStart = () => {};
+
+  useEffect(() => {
+    setTimeout(
+      () => (action === "add" ? handleAddItem() : handlRemoveItems()),
+      1000
+    );
+  });
+
+  return (
+    <ul className="list">
+      <RenderCountLabel label="List" count={counter} />
+      <br />
+      <ChangeModeButton action={action} onClick={handleChangeAction} />
+      <br />
+      <div className="btn-actions">
+        <AddToStartButton onClick={handleAddToStart} />
+        <AddToEndButton onClick={handleAddItem} />
+      </div>
+      {items.map((item, index) => (
+        <ListItem
+          key={item}
+          item={item}
+          index={index}
+          onRemove={handleRemoveItem}
+        />
+      ))}
+    </ul>
+  );
+};
+
+function App() {
+  return (
+    <div>
+      <List />
     </div>
   );
 }
+
+export default App;
